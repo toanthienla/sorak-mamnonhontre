5cb403c6d64eb22e7249784de1be639296db141d
qnpyen2004 <quang.nhatphiyen2004@gmail.com>
2026-07-18 11:12:20 +0700

    feat(assessment-themes): add list endpoint

---
+++ sorak-api/src/controllers/assessment-themes.controller.js
@@ -0,0 +1,25 @@
+import * as svc from '../services/assessment-themes.service.js';
+
+export async function findAll(req, res) {
+  res.paginated(await svc.findAll(req.query));
+}
+
+export async function findOne(req, res) {
+  res.success(await svc.findOne(Number(req.params.id)));
+}
+
+export async function create(req, res) {
+  res.success(await svc.create(req.body, req.user.sub));
+}
+
+export async function update(req, res) {
+  res.success(await svc.update(Number(req.params.id), req.body, req.user.sub));
+}
+
+export async function setStatus(req, res) {
+  res.success(await svc.setStatus(Number(req.params.id), req.body.is_active, req.user.sub));
+}
+
+export async function remove(req, res) {
+  res.success(await svc.hardDelete(Number(req.params.id)));
+}
